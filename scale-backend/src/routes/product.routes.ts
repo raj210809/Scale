c3275c789ada49f9ae50f216dd909c6b72e43e9b
Talha Ansari <154398538+amsorrytola@users.noreply.github.com>
2025-01-23 20:12:56 +0530

    Update product.routes.ts

--- scale-backend/src/routes/product.routes.ts
+++ scale-backend/src/routes/product.routes.ts
@@ -1,8 +1,12 @@
 import express from "express";
-import { getPresignedUrls } from "src/controllers/product.controller";
+import { getPresignedUrls,writeReviews,addProducts,getProducts,viewProduct } from "../controllers/product.controller";
 
 const router = express.Router();
 
 router.post("/get-presignedurls" , getPresignedUrls)
+router.post("/add-products",addProducts)
+router.get("/get-products",getProducts)
+router.get("/view-product/:product_id",viewProduct)
+router.post("/write-reviews", writeReviews)
 
 export default router;
